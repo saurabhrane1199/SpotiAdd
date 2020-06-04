6802ae09195df4df2d1eee3b1ef0e28da4f34c75
saurabhrane1199 <36326161+saurabhrane1199@users.noreply.github.com>
2020-06-05 03:00:17 +0530

    OAuth Expiration Added

--- frontend/popup.js
+++ frontend/popup.js
@@ -20,6 +20,15 @@ function setRefTokenFromStorage(value){
   });
 }
 
+function setauthTokenFromStorage(value){
+  let d = (new Date()).toString()
+  chrome.storage.sync.set({"auth_token": value,
+                           "created_time" : d,
+                        }, function() {
+    console.log('Refresh_Token is set to ' + value);
+  });   
+}
+
 
 //get OAuthCode
 function getAuthCode(code,isRefreshToken){
@@ -29,6 +38,7 @@ function getAuthCode(code,isRefreshToken){
     .then(result =>{
       let jsonRes = JSON.parse(result);
       spotify_access_token = jsonRes["oauth"];
+      setauthTokenFromStorage(spotify_access_token);
       refresh_token = jsonRes["refresh_token"];
       if(refresh_token != "not_defined"){
         setRefTokenFromStorage(refresh_token);
@@ -41,6 +51,8 @@ function getAuthCode(code,isRefreshToken){
 
 //API CALL
 function getSongs(id){
+  document.getElementById("searchSongs").remove();
+  document.getElementById("songs").style.visibility = "visible";
   fetch('http://127.0.0.1:5000/'+id+'?code='+spotify_access_token)
     .then(r => r.text())
     .then(result =>{
@@ -108,21 +120,34 @@ function addSongButtonListener(){
 window.addEventListener('load', () => {
   // refresh_token = getRefTokenFromStorage();
   addAuthButtonListener();
-  addSubmitButtonListener();
-  chrome.storage.sync.get(["refresh_token"], function(result) {
+  chrome.storage.sync.get(["auth_token","created_time","refresh_token"],function(result){
+    console.log('Auth_Token currently is ' + result.auth_token);
+    console.log('Created_Time currently is ' + result.created_time);
     console.log('Refresh_Token currently is ' + result.refresh_token);
-    refresh_token = result.refresh_token;
-    if(refresh_token != undefined){
-      getAuthCode(refresh_token, "1");
+    let currentTime = new Date()
+    let createdDate = new Date(result.created_time)
+    let diffMins = (currentTime.getTime() - createdDate.getTime())/1000 
+    console.log(diffMins,"Difference in minutes")
+    if(result.auth_token != undefined && diffMins<3600){
+      spotify_access_token = result.auth_token
       document.getElementById("authorizationBlock").remove();
       document.getElementById("searchSongs").style.visibility = "visible";  
     }
+    else if(result.refresh_token != undefined){
+      console.log("Refresh_Tag is defined")
+      document.getElementById("authorizationBlock").remove();
+      document.getElementById("searchSongs").style.visibility = "visible";
+      getAuthCode(result.refresh_token, "1");
+    }
     else{
+      
       document.getElementById("searchSongs").style.visibility = "hidden";
     }
     document.getElementById("songs").style.visibility = "hidden";
-    document.getElementById("notMusic").style.visibility = "hidden";
+    document.getElementById("notMusic").style.visibility = "hidden"
   });
+  
+  addSubmitButtonListener();
 });
 
 function showNotAMusicVideo(){
@@ -132,13 +157,11 @@ function showNotAMusicVideo(){
 
 
 function addSongsToUI(response){
-
-  document.getElementById("searchSongs").remove();
-  document.getElementById("songs").style.visibility = "visible";
   let nameArr = response.names;
   let artArr = response.artists;
   let imgArr = response.images;
   let uriArr = response.uri;
+  document.getElementById("loaderDiv").remove();
 
   let songsTable = document.getElementById("tbody");
 
@@ -149,11 +172,11 @@ function addSongsToUI(response){
     let uriCell = row.insertCell(2);
 
     nameCell.innerHTML = `<span style="padding-right:3px; padding-top: 3px; display:inline-block;"> 
-                            <img class="iconImg" src="${imgArr[i]}"></img>
+                            <img class="iconImg" src="${imgArr[i]}" width="40px"></img>
                             <p>${nameArr[i]}</p>
                           </span>`;
     artistCell.innerHTML = `<p>${artArr[i]}</p>`;
-    uriCell.innerHTML = `<button class="btn addSongs" id="addSong${i}" href="${uriArr[i]}" style="background-color : transparent"><i class="fa fa-plus" style="color:white;"></i></button>`;
+    uriCell.innerHTML = `<button class="btn addSongs" id="addSong${i}" href="${uriArr[i]}" style="background-color : transparent"><i class="fa fa-plus" style="color:white;w"></i></button>`;
   }
   addSongButtonListener();
 
